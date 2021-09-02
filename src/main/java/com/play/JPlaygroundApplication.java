4bb4fdebc003f4421d1f3725aea37573bb03b05e
masterscode <ogbinakaemmanuel@gmail.com>
2021-09-02 08:39:39 +0100

    init commit

---
+++ src/main/java/com/play/JPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPlaygroundApplication.class, args);
+	}
+
+}
