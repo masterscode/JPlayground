c15b858312c585ad04e78f61a7d33ff57142abaf
emmanuel ogbinaka <ogbinakaemmanuel@gmail.com>
2022-07-29 10:52:24 +0100

    test java promise alternative

--- src/main/java/com/play/controllers/TestController.java
+++ src/main/java/com/play/controllers/TestController.java
@@ -1,23 +1,30 @@
 package com.play.controllers;
 
-import com.play.services.interfaces.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.stream.IntStream;
+import com.play.services.interfaces.UserService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
     private final UserService userService;
 
     @GetMapping("/async")
     public String getAsync(){
-        userService.logNumbersAsync();
+        CompletableFuture.runAsync(()->{
+            IntStream.range(1, 10000000)
+            .forEach(num ->log.info(String.valueOf(num)));
+        });
         return "async code started";
     }
 
